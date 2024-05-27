7adb09e5fc674ce89d9ce72a079474ef723c7469
kjkim9612 <kjkim9612@gmail.com>
2024-05-27 20:23:10 +0900

    24.05.27 modify comment

--- MovAvgFilter/MovAvgFilter.m
+++ MovAvgFilter/MovAvgFilter.m
@@ -5,9 +5,10 @@ function avg = MovAvgFilter(x)
   persistent firstRun
 
   if isempty(firstRun)
-    n = 10;
-    xbuf = x * ones(n+1, 1); % n이 10이므로, 1로된 11행 1열 행렬을 만단다.
-                             % 결과 : 1로된 11행 1열 행렬에 입력된 x를 곱한값
+    n = 10;                   % 데이터 갯수 지정
+    xbuf = x * ones(n+1, 1);  % 내부 버퍼 초기화
+                              % n이 10이므로, 1로된 11행 1열 행렬을 만단다.
+                              % 결과 : 1로된 11행 1열 행렬에 입력된 x를 곱한값
 
     k = 1;
     prevAvg = x;
